e430c9c693a42b59bc6a4077e58699c04e7640fa
anton <antonia_zeilinger@hotmail.com>
2021-08-31 19:49:21 +0200

    savev

--- vacina-backend/app/Models/Impfung.php
+++ vacina-backend/app/Models/Impfung.php
@@ -1,5 +1,7 @@
 <?php
 
+
+// PUSH
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
--- vacina-frontend/src/app/impfung-form/impfung-form.component.ts
+++ vacina-frontend/src/app/impfung-form/impfung-form.component.ts
@@ -120,7 +120,7 @@ export class ImpfungFormComponent implements OnInit {
         }
     }
 
-
+// save
 
 
 
